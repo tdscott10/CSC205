8f112b8f617097daafa5990bb42dca3e9aa293de
AaronStahley <stahleyaaron@yahoo.com>
2015-04-21 20:14:46 -0700

    Starting project 8

---
+++ Project8/SearchTreeDriver.java
@@ -0,0 +1,15 @@
+//Program Name : SearchTreeDriver
+//Author : Aaron Stahley	
+//Date Written: 4/21/2015
+//Class : SearchTreeDriver.java
+
+package Project8;
+
+public class SearchTreeDriver {
+
+
+	public static void main(String[] args) {
+
+	}
+
+}
